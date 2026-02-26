23ad0f7ed8d67d04f0960c04bd92293ab2000193
Athul Sudheer <athulsudheer2007@gmail.com>
2026-02-27 00:50:26 +0530

    Fix domain typo in ALLOWED_HOSTS

--- azurebay_backend/azurebay_backend/settings.py
+++ azurebay_backend/azurebay_backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-...'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "azurebay_backend-94b8.onrender.com",
+    "azurebay-backend-94b8.onrender.com",
     "localhost",
     "127.0.0.1",
 ]
