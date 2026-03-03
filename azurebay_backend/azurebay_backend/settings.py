289d99f9c1c946055c9d111c30e598a071f57495
Athul Sudheer <athulsudheer2007@gmail.com>
2026-03-03 21:49:02 +0530

    Move HTML to templates folder

--- azurebay_backend/azurebay_backend/settings.py
+++ azurebay_backend/azurebay_backend/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'azurebay_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR.parent],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
