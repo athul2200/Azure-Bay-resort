7d1df5b26e8d8d459f36ffb5caabacb4585a5471
Athul Sudheer <athulsudheer2007@gmail.com>
2026-03-03 21:22:43 +0530

    deployed

--- azurebay_backend/azurebay_backend/settings.py
+++ azurebay_backend/azurebay_backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["azure-bay-resort.onrender.com"]
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
